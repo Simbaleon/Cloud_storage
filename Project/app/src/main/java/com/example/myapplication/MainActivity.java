869ad2c1339178979c4a0683e6365d2dd35067d4
Никита <nicitazaletin@yandex.ru>
2021-03-04 02:19:17 +0300

    Разрешение на работу с файлами

--- Project/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Project/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.myapplication;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+
+import android.content.ClipboardManager;
+
+import android.content.Context;
+
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.yanzhenjie.permission.AndPermission;
+import com.yanzhenjie.permission.runtime.Permission;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private ImageView imageView;
+    private ClipboardManager clipboard;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Permissions
+        SetPermission();
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView);
+        clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+    }
+
+    @SuppressLint("WrongConstant")
+    void SetPermission(){
+        AndPermission.with(this)
+                .runtime()
+                .permission(Permission.Group.STORAGE)
+                .onGranted(permissions -> {
+                    // Storage permission are allowed.
+                })
+                .onDenied(permissions -> {
+                    // Storage permission are not allowed.
+                    this.finish();
+                })
+                .start();
+
     }
 }
